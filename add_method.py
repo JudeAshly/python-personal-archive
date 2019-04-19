b79d93db05fe8044725f0ddb1b454a0e846be125
Jude Ashly <judeashlytherock@gmail.com>
2019-04-19 21:02:39 +0530

    add method added

---
+++ add_method.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on 
+
+Course work: 
+
+@author: Jude 
+
+Source:
+    
+'''
+
+# Import necessary modules
+ 
+
+
+def startpy():
+    a = 5
+    b = 4
+    c = a + b
+
+    print(c)
+
+
+if __name__ == '__main__':
+    startpy()
